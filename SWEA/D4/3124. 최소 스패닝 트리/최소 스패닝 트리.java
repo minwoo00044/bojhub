936fc4b9ef8244b7b8a149d3302cdd3db8fb8c3e
minwoo00044 <70777372+minwoo00044@users.noreply.github.com>
2026-03-11 17:00:27 +0900

    [D4] Title: 최소 스패닝 트리, Time: 3,889 ms, Memory: 166,032 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,55 +1,61 @@
 
 import java.util.*;
+
+
 import java.io.*;
 class Solution
 {
-	static int n,p[],edges[][];
-	static long ans;
-	public static void main(String args[]) throws Exception
-	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T;
-		T=Integer.parseInt(br.readLine());
-		for(int test_case = 1; test_case <= T; test_case++)	{
-			StringTokenizer st = new StringTokenizer(br.readLine()," ");
-			int v= Integer.parseInt(st.nextToken());
-			int e= Integer.parseInt(st.nextToken());
-			p = new int[v];
-			ans = 0;
-			for(int i = 0; i < v; i++) {
-				p[i] = i;
-			}
-			edges = new int[e][];
-			for(int i = 0; i < e;i ++) {
-				st = new StringTokenizer(br.readLine()," ");
-				edges[i] = new int[] {Integer.parseInt(st.nextToken())-1,Integer.parseInt(st.nextToken())-1,Integer.parseInt(st.nextToken())};
-			
-			}
-			Arrays.sort(edges, (a,b)-> a[2]-b[2]);
-			int cnt = 0;
-			for(int i = 0; i < e; i++) {
-				if(union(edges[i][0], edges[i][1])) {
-					ans+=edges[i][2];
-					cnt++;
-				}
-				if(cnt == v-1) {
-					break;
-				}
-			}
-			
-			System.out.println("#"+test_case+" "+ans);
+    static int n;
+    static double xs[],ys[];    
+	static class Vertex implements Comparable<Vertex>{
+		public Vertex(int no, double weight) {
+			super();
+			this.no = no;
+			this.weight = weight;
 		}
+		int no;
+		double weight;
+		@Override
+		public int compareTo(Vertex o) {
+			return Double.compare(this.weight, o.weight);
+		}
+		
 	}
-	private static boolean union(int a, int b) {
-		a = findSet(a);
-		b = findSet(b);
-		if(a==b) return false;
-		p[b] = a;
-		return true;
-	}
-	private static int findSet(int a) {
-		if(p[a] == a) return a;
-		return p[a] = findSet(p[a]);
-	}
-
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T;
+        T=Integer.parseInt(br.readLine());
+        for(int test_case = 1; test_case <= T; test_case++)  {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            int v= Integer.parseInt(st.nextToken());
+            int e= Integer.parseInt(st.nextToken());
+            long ans = 0;
+            List<Vertex>[] adj = new ArrayList[v];
+            for(int i = 0; i < v; i++) {
+            	adj[i] = new ArrayList<Vertex>();
+            }
+            for(int i = 0; i < e;i ++) {
+                st = new StringTokenizer(br.readLine()," ");
+                int from = Integer.parseInt(st.nextToken())-1;
+                int to = Integer.parseInt(st.nextToken())-1;
+                int w = Integer.parseInt(st.nextToken());
+                adj[from].add(new Vertex(to, w));
+                adj[to].add(new Vertex(from, w));
+            }
+            PriorityQueue<Vertex> pq = new PriorityQueue<>();
+            boolean[] visited= new boolean[v];
+            pq.offer(new Vertex(0, 0));
+            int pick = 0;
+            while(!pq.isEmpty()) {
+            	Vertex vt = pq.poll();
+            	if(visited[vt.no])continue;
+            	ans+=vt.weight;
+            	visited[vt.no] = true;
+            	if(++pick == v) break;
+            	pq.addAll(adj[vt.no]);
+            }
+            System.out.println("#"+test_case+" "+ans);
+        }
+    }
 }
