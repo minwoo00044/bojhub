0d6b167b193e75b56cc91d9649df5e78dfdfcfd0
minwoo00044 <70777372+minwoo00044@users.noreply.github.com>
2026-03-11 16:48:41 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 920 ms, Memory: 187,156 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,63 +1,73 @@
 
 import java.util.*;
+
+
 import java.io.*;
 class Solution
 {
-	static int n;
-	static double p[],xs[],ys[];
-	static List<double[]> edges;
-	public static void main(String args[]) throws Exception
-	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T;
-		T=Integer.parseInt(br.readLine());
-		for(int test_case = 1; test_case <= T; test_case++)	{
-			n= Integer.parseInt(br.readLine());
-			StringTokenizer st;
-			p = new double[n];
-			for(int i = 0; i <n;i++) {
-				p[i] = i;
-			}
-			xs = new double[n];
-			ys = new double[n];
-			edges = new ArrayList<double[]>();
-			st = new StringTokenizer(br.readLine()," ");
-
-			for(int i = 0; i < n; i++) {
-				xs[i] = Double.parseDouble(st.nextToken());
-			}
-			st = new StringTokenizer(br.readLine()," ");
-			for(int i = 0; i < n; i++) {
-				ys[i] = Double.parseDouble(st.nextToken());
-			}
-			double l = Double.parseDouble(br.readLine());
-			double ans = 0;
-			for(int i = 0; i < n; i++) {
-				for(int j = i+1; j < n; j++) {
-                    edges.add(new double[] {i,j, Math.pow(xs[j]-xs[i],2)+Math.pow(ys[j]-ys[i],2)});
-				}
-			}
-			Collections.sort(edges, (a,b)-> Double.compare(a[2], b[2]));
-			int cnt = 0;
-            for(int i = 0; i < edges.size(); i++) {
-				if(union(edges.get(i)[0],edges.get(i)[1])) {
-					ans += edges.get(i)[2];
-					cnt++;
-                }
-                if(cnt == n-1) break;
-			}
-			System.out.println("#"+test_case+" "+Math.round(ans * l) );
+    static int n;
+    static double xs[],ys[];    
+	static class Vertex implements Comparable<Vertex>{
+		public Vertex(int no, double weight) {
+			super();
+			this.no = no;
+			this.weight = weight;
 		}
+		int no;
+		double weight;
+		@Override
+		public int compareTo(Vertex o) {
+			return Double.compare(this.weight, o.weight);
+		}
+		
 	}
-	static boolean union(double a, double b) {
-		a = findSet(a);
-		b = findSet(b);
-		if(a == b) return false;
-		p[(int)b] = a;
-		return true;
-	}
-	private static double findSet(double a) {
-		if(p[(int)a] == a) return a;
-		return p[(int)a] = findSet(p[(int)a]);
-	}
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T;
+        T=Integer.parseInt(br.readLine());
+        for(int test_case = 1; test_case <= T; test_case++)  {
+            n= Integer.parseInt(br.readLine());
+            StringTokenizer st;
+    		List<Vertex>[] adj = new ArrayList[n];
+    		for(int i = 0; i < n; i++) {
+    			adj[i] = new ArrayList<>();
+    		}
+            xs = new double[n];
+            ys = new double[n];
+            st = new StringTokenizer(br.readLine()," ");
+ 
+            for(int i = 0; i < n; i++) {
+                xs[i] = Double.parseDouble(st.nextToken());
+            }
+            st = new StringTokenizer(br.readLine()," ");
+            for(int i = 0; i < n; i++) {
+                ys[i] = Double.parseDouble(st.nextToken());
+            }
+            double l = Double.parseDouble(br.readLine());
+            double ans = 0;
+            for(int i = 0; i < n; i++) {
+            	for(int j = 0; j < n; j++) {
+            		adj[i].add(new Vertex(j, Math.pow(xs[j]-xs[i],2)+Math.pow(ys[j]-ys[i],2)));
+            		adj[j].add(new Vertex(i, Math.pow(xs[j]-xs[i],2)+Math.pow(ys[j]-ys[i],2)));
+            	}
+            }
+    		boolean[] visited = new boolean[n];
+    		PriorityQueue<Vertex> pq = new PriorityQueue<>();
+    		int pick = 0;
+    		pq.offer(new Vertex(0, 0));
+    		
+    		while(!pq.isEmpty()) {
+    			Vertex v= pq.poll();
+    			if(visited[v.no]) continue;
+    			ans+= v.weight;
+    			visited[v.no] = true;
+    			if(++pick == n) break;
+    			pq.addAll(adj[v.no]);
+    		}
+    		ans *= l;
+            System.out.println("#"+test_case+" "+Math.round(ans));
+
+        }
+    }
 }
